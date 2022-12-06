6c07caba9aa5cc0c6c0123c719c934d8492b7ef3
Linssi Colin Ruiz <linssi101@gmail.com>
2022-12-06 17:25:41 -0600

    cannot get 404 working again.

--- server/public/scripts/scripts.js
+++ server/public/scripts/scripts.js
@@ -1,5 +1,44 @@
 $(document).ready(onReady);
 
-function onReady(){
-    console.log('in onReady');
+function onReady() {
+	console.log("in onReady");
+
+	// operators
+	$(".add").on("click", addition);
+	$(".subtract").on("click", subtraction);
+	$(".multiply").on("click", multiplication);
+	$(".divide").on("click", division);
+
+	// clear input fields
+	$(".clear-inputs").on("click", clearInputs);
+}
+// selected operator
+let operatorSelected;
+
+function addition() {
+	operatorSelected = "+";
+}
+function subtraction() {
+	operatorSelected = "-";
+}
+function multiplication() {
+	operatorSelected = "*";
+}
+function division() {
+	operatorSelected = "/";
+}
+
+function clearInputs() {
+	$(".user-input-first-number").val("");
+	$(".user-input-second-number").val("");
+}
+
+let userInput;
+
+function submitValues() {
+	userInput = {
+		firstNumber: $(".user-input-first-number"),
+		secondNumber: $(".user-input-second-number"),
+	};
+	// postCalculation();
 }
